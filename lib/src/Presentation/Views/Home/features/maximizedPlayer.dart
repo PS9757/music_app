f4fee33197e4406360016abefdc21a2b0d741d54
PS9757 <priyanshusingh48461@gmail.com>
2023-11-01 11:28:33 +0530

    search

--- lib/src/Presentation/Views/Home/features/maximizedPlayer.dart
+++ lib/src/Presentation/Views/Home/features/maximizedPlayer.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
@@ -20,7 +21,8 @@ class MaximizedPlayerScreen extends StatefulWidget {
     required this.name,
     required this.photo,
     required this.url,
-    required this.identifier, required this.playlist,
+    required this.identifier,
+    required this.playlist,
   });
 
   @override
@@ -30,21 +32,11 @@ class MaximizedPlayerScreen extends StatefulWidget {
 class _MaximizedPlayerScreenState extends State<MaximizedPlayerScreen> {
   var audioTrack;
 
-  // @override
-  // void didChangeDependencies() {
-  //   super.didChangeDependencies();
-  //   initializeAudioSource();
-  // }
-  // void initializeAudioSource() {
-  //   final audioPlayerProvider = Provider.of<AudioPlayerProvider>(context, listen: true);
-  //   audioPlayerProvider.setAudioSource(widget.playlist, widget.identifier);
-  // }
-
   @override
   Widget build(BuildContext context) {
     // Create an AudioTrack for the selected song
     audioTrack = AudioTrack(
-      widget.identifier,  // Replace with a unique ID or appropriate identifier
+      widget.identifier, // Replace with a unique ID or appropriate identifier
       title: widget.name,
       artist: widget.artist,
       duration: Duration(seconds: 180), // Replace with the actual duration
@@ -52,13 +44,71 @@ class _MaximizedPlayerScreenState extends State<MaximizedPlayerScreen> {
       networkUrl: widget.url,
     );
     // print("================================${audioTrack.networkUrl}");
-    final audioPlayerProvider = Provider.of<AudioPlayerProvider>(context, listen: true);
+    final audioPlayerProvider =
+        Provider.of<AudioPlayerProvider>(context, listen: false);
     audioPlayerProvider.setSingleAudioSource(audioTrack);
-    // Set the audioTrack in AudioPlayerProvider
 
+    // Set the audioTrack in AudioPlayerProvider
 
     return MaximizedMusicPlayer(
       albumCover: Image.network(widget.photo),
+      playPauseButton: IconButton(
+        icon: audioPlayerProvider.isPlaying
+            ? Icon(
+                Icons.pause,
+                color: Colors.white,
+                size: 25,
+              )
+            : Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 25,
+              ),
+        onPressed: () {
+          // Handle play/pause button action here
+          setState(() {
+            if (audioPlayerProvider.isPlaying) {
+                audioPlayerProvider.playerState = PlayerState.paused;
+            } else {
+              audioPlayerProvider.playSong();
+            }
+          });
+        },
+      ),
+      nextButton: IconButton(
+        icon: Icon(
+          Icons.skip_next,
+          color: Colors.white,
+          size: 25,
+        ),
+        onPressed: () {
+          //fluttertoast
+          Fluttertoast.showToast(
+            msg: "Soon Available",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.TOP,
+            backgroundColor: Colors.white,
+            textColor: Colors.black,
+          );
+        },
+      ),
+      previousButton: IconButton(
+        icon: Icon(
+          Icons.skip_previous,
+          color: Colors.white,
+          size: 25,
+        ),
+        onPressed: () {
+          //fluttertoast
+          Fluttertoast.showToast(
+            msg: "Soon Available",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.TOP,
+            backgroundColor: Colors.white,
+            textColor: Colors.black,
+          );
+        },
+      ),
       playerCustomizer: NamedPlayerCustomizer(
         // songTitleRowRight: Text(widget.name),
         midRowFarLeft: IconButton(
@@ -66,7 +116,7 @@ class _MaximizedPlayerScreenState extends State<MaximizedPlayerScreen> {
           onPressed: () {
             print("hjdsvjf");
             Fluttertoast.showToast(
-              msg: "Added to Favourites",
+              msg: "Soon Available",
               toastLength: Toast.LENGTH_SHORT,
               gravity: ToastGravity.TOP,
               backgroundColor: Colors.white,
@@ -78,15 +128,36 @@ class _MaximizedPlayerScreenState extends State<MaximizedPlayerScreen> {
           icon: Icon(Icons.shuffle),
           onPressed: () {
             // Handle shuffle button action here
+            Fluttertoast.showToast(
+              msg: "Soon Available",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.TOP,
+              backgroundColor: Colors.white,
+              textColor: Colors.black,
+            );
           },
         ),
         midRowRight: IconButton(
           icon: Icon(Icons.repeat),
           onPressed: () {
             // Handle repeat button action here
+            Fluttertoast.showToast(
+              msg: "Soon Available",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.TOP,
+              backgroundColor: Colors.white,
+              textColor: Colors.black,
+            );
           },
         ),
       ),
+      // playPauseButton: IconButton(
+      //   icon: Icon(Icons.play_arrow),
+      //   onPressed: () {
+      //
+      //     // Handle play/pause button action here
+      //   },
+      // ),
       backgroundDecoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topLeft,
@@ -100,5 +171,4 @@ class _MaximizedPlayerScreenState extends State<MaximizedPlayerScreen> {
       ),
     );
   }
-
 }
